06d848f2a47f4493e33bf18513a7ed4d7358f833
Cmrdeveloper <cmrbolsadevelopment@gmail.com>
2023-08-17 22:02:12 +0100

    continuación

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { themeSettings } from "../theme";
-
-
+import Navbar from "@/scenes/navbar";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -15,6 +14,7 @@ function App() {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />    
             <Routes>
               <Route path="/" element={<div>dashboard page</div>} />
               <Route path="/predictions" element={<div>predictions page</div>} />
