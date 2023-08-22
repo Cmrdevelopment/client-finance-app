aec691b9a451d22505de9d0aa080b9eb964e2f3c
Cmrdeveloper <cmrbolsadevelopment@gmail.com>
2023-08-22 23:13:11 +0100

    Row2

--- src/scenes/dashboard/Row2.tsx
+++ src/scenes/dashboard/Row2.tsx
@@ -12,9 +12,12 @@ import {
   Pie, 
   PieChart, 
   ResponsiveContainer, 
+  Scatter, 
+  ScatterChart, 
   Tooltip, 
   XAxis, 
-  YAxis 
+  YAxis, 
+  ZAxis
 } from 'recharts';
 
 const pieData = [
@@ -39,6 +42,21 @@ const Row2 = () => {
       })
     );
   }, [operationalData]);
+
+  const productExpenseData = useMemo(() => {
+    return (
+      productData &&
+      productData.map(({ _id, price, expense }) => {
+        return {
+          id: _id,
+          price: price,
+          expense: expense,
+        };
+      })
+    );
+  }, [productData]);
+
+
   return (
     <>
     <DashboardBox gridArea="d">
@@ -144,7 +162,46 @@ const Row2 = () => {
           </Box>
       </FlexBetween>
     </DashboardBox>
-    <DashboardBox gridArea="f"></DashboardBox>
+    <DashboardBox gridArea="f">
+    <BoxHeader title="Product Prices vs Expenses" sideText="+4%" />
+    <ResponsiveContainer width="100%" height="100%">    
+        <ScatterChart
+          margin={{
+            top: 20,
+            right: 25,
+            bottom: 40,
+            left: -10,
+          }}
+        >
+          <CartesianGrid stroke={palette.grey[800]} />
+          <XAxis 
+            type="number"
+            dataKey="price"
+            name="price"
+            axisLine={false}
+            tickLine={false}
+            style={{ fontSize: "10px" }}
+            tickFormatter={(v) => `$${v}`}
+            />
+          <YAxis
+              type="number"
+              dataKey="expense"
+              name="expense"
+              axisLine={false}
+              tickLine={false}
+              style={{ fontSize: "10px" }}
+              tickFormatter={(v) => `$${v}`}
+            />
+          <ZAxis type="number" range={[20]} />
+            <Tooltip formatter={(v) => `$${v}`} />
+            <Scatter
+              name="Product Expense Ratio"
+              data={productExpenseData}
+              fill={palette.tertiary[500]}
+            />
+          </ScatterChart>
+        </ResponsiveContainer>
+      </DashboardBox>
     </>
   )
 }
