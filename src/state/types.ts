7de61ee78513624528c61757b3767befdacb5ace
Cmrdeveloper <cmrbolsadevelopment@gmail.com>
2023-08-22 02:52:47 +0100

    3.32.10

--- src/state/types.ts
+++ src/state/types.ts
@@ -33,17 +33,17 @@ export interface GetKpisResponse {
     createdAt: string;
     updatedAt: string;
 }
-  
-// export interface GetProductsResponse {
-//     id: string;
-//     _id: string;
-//     __v: number;
-//     price: number;
-//     expense: number;
-//     transactions: Array<string>;
-//     createdAt: string;
-//     updatedAt: string;
-// }
+
+export interface GetProductsResponse {
+    id: string;
+    _id: string;
+    __v: number;
+    price: number;
+    expense: number;
+    transactions: Array<string>;
+    createdAt: string;
+    updatedAt: string;
+}
   
 //   export interface GetTransactionsResponse {
 //     id: string;
